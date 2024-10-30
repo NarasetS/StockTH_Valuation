a657548e2d98b88a111c9dfe8d9f42ab8b67c839
NarasetS <80676034+NarasetS@users.noreply.github.com>
2024-10-30 14:53:53 +0700

    dd

--- pages/1_dcf_calculator.py
+++ pages/1_dcf_calculator.py
@@ -27,7 +27,7 @@ if sector == 'Financial Services':
 else:
     totalnoncurrentliabilities = data.balancesheet.loc[data.balancesheet.index == 'Total Non Current Liabilities Net Minority Interest'].values[0][0] - data.balancesheet.loc[data.balancesheet.index == 'Cash And Cash Equivalents'].values[0][0]
 input_totalnoncurrentliabilities = st.sidebar.number_input(
-    "total non-curren tliabilities : ", value=totalnoncurrentliabilities , placeholder= "Input total non-current liabilities.."
+    "total non-current liabilities : ", value=totalnoncurrentliabilities , placeholder= "Input total non-current liabilities.."
 )
 
 ## Average Free Cash Flow ## 
@@ -89,12 +89,72 @@ def intrinsic_value(
     return (np.sum(PV_1) + np.sum(PV_2) + TV - totalnoncurrentliabilities)/shares
 ## DCF Calculation Function ##
 
-#### Main area #############################################################################################
-st.markdown('To perform discounted cash flow to find intrinsic value for : ' + ticker)
-iv = np.round(intrinsic_value(
+## Reverse DCF Function ##
+def intrinsic_value_diff_currentprice(
         shares,
         totalnoncurrentliabilities,
         averagefreecashflow,
+        year_growth,
+        year_sustain,
+        growth_to_sustain_ratio,
+        dr,
+        tgr,
+        currentprice,
+        gr
+        ):
+    
+    PV_1 = [averagefreecashflow * ((1+gr)**i) / ((1+dr)**i) for i in range(1,year_growth+1,1)]
+    PV_2 = [averagefreecashflow * ((1+gr)**year_growth) * ((1+(gr*growth_to_sustain_ratio))**i)/((1+dr)**(year_growth+i)) for i in range(1,year_sustain+1,1)]
+    TV = averagefreecashflow * ((1+gr)**year_growth) * ((1+(gr*growth_to_sustain_ratio))**(year_sustain)) * (1+tgr) / ((dr-tgr)*((1+dr)**(year_growth+year_sustain)))
+    # print((((np.sum(PV_1) + np.sum(PV_2) + TV - totalnoncurrentliabilities)/shares)) , currentprice)
+    return (((np.sum(PV_1) + np.sum(PV_2) + TV - totalnoncurrentliabilities)/shares) - currentprice)/currentprice
+
+def newton_raphson_method(
+            shares,
+            totalnoncurrentliabilities,
+            averagefreecashflow,
+            year_growth,
+            year_sustain,
+            growth_to_sustain_ratio,
+            dr,
+            tgr,
+            currentprice):
+    
+    initial_guess = 0.01
+    tolerance = 0.01
+    max_iterations = 1000000
+    x_n = initial_guess
+    
+    for iteration in range(max_iterations):
+    # while (True):
+        f_x = intrinsic_value_diff_currentprice(
+            shares,
+            totalnoncurrentliabilities,
+            averagefreecashflow,
+            year_growth,
+            year_sustain,
+            growth_to_sustain_ratio,
+            dr,
+            tgr,
+            currentprice,
+            x_n
+        )
+
+        if abs(f_x) < tolerance :
+            break
+
+        x_n = x_n - f_x * 0.01
+
+    return x_n
+## Reverse DCF Function ##
+
+
+#### Main area #############################################################################################
+st.markdown('To perform discounted cash flow to find intrinsic value for : ' + ticker)
+iv = np.round(intrinsic_value(
+        input_shares,
+        input_totalnoncurrentliabilities,
+        input_averagefreecashflow,
         input_year_growth,
         input_year_sustain,
         input_growth_to_sustain_ratio,
@@ -103,11 +163,72 @@ iv = np.round(intrinsic_value(
         input_growthrate
         ),2)
 st.markdown('intrinsic value = ' + str(iv))
-
-st.markdown('current price : ' + str(data.info['currentPrice']))
+currentprice = data.info['currentPrice']
+st.markdown('current price : ' + str(currentprice))
 st.markdown('margin of safety (%) : ' + str(
     np.round((data.info['currentPrice'] - iv)*100/data.info['currentPrice'],2)
 ))
 
+try:
+    st.markdown('profitMargins : ' + str(data.info['profitMargins']))
+    st.markdown('returnOnEquity : ' + str(data.info['returnOnEquity']))
+    st.markdown('trailingPE : ' + str(data.info['trailingPE']))
+    st.markdown('priceToBook : ' + str(data.info['priceToBook']))
+    st.markdown('debtToEquity (%) : ' + str(data.info['debtToEquity']))
+    st.markdown('currentRatio : ' + str(data.info['currentRatio']))
+    st.markdown('pegRatio : ' + str(data.info['pegRatio']))
+    
+except:
+    st.markdown('Theres error loading indicative ratios')
 
-#### Main area #############################################################################################
+
+st.header('Reverse DCF Curve (Expected of FCF vs Discount Rate) based on current price')
+st.markdown('To indicate expected growth rate based on particular discount rate which makes intrinsic value calculated from DCF equal to current price..')
+
+## Perform Reverse DCF ##
+dr_list = [dr*0.01 for dr in range(int((input_terminalgrowthrate*100)+1),101,1)]
+df = pd.DataFrame(index=dr_list,columns=['growthrate'])
+growthrate = [ newton_raphson_method(           
+            input_shares,
+            input_totalnoncurrentliabilities,
+            input_averagefreecashflow,
+            input_year_growth,
+            input_year_sustain,
+            input_growth_to_sustain_ratio,
+            dr,
+            input_terminalgrowthrate,
+            currentprice) for dr in dr_list]
+df['growthrate'] = growthrate
+df.reset_index(inplace=True)
+df = df.rename(columns={'index':'discountrate'})
+st.scatter_chart(df, x = 'discountrate', y = 'growthrate')
+## Perform Reverse DCF ##
+
+## test ####
+# root = newton_raphson_method(           
+#             input_shares,
+#             input_totalnoncurrentliabilities,
+#             input_averagefreecashflow,
+#             input_year_growth,
+#             input_year_sustain,
+#             input_growth_to_sustain_ratio,
+#             input_discountrate,
+#             input_terminalgrowthrate,
+#             currentprice)
+# st.markdown(root)
+# diff = intrinsic_value_diff_currentprice(
+#         input_shares,
+#         input_totalnoncurrentliabilities,
+#         input_averagefreecashflow,
+#         input_year_growth,
+#         input_year_sustain,
+#         input_growth_to_sustain_ratio,
+#         input_discountrate,
+#         input_terminalgrowthrate,
+#         currentprice,
+#         root
+#         ) * 100
+# st.markdown(diff)
+## test ####
+
+#### Main area #############################################################################################0
---
+++ pages/2_reverse_dcf_ranking.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from datetime import date
+
+
+
+st.header('Coming Soon !!! ')
