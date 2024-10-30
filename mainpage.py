71a5fb28fa09fd367a99093bdc8ec2ef0d8b2521
NarasetS <80676034+NarasetS@users.noreply.github.com>
2024-10-30 15:00:20 +0700

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header('Finding -Intrinsic Value- using DCF Method with the concept as the pic shown below')
 st.markdown('In addition, you would be able to perform a reverse DCF and try ranking it by the least expected growth with a certain Discount Rate which would make the intrinsic value equal to current price')
-st.image("\\Image.jfif", caption="DCF Concept in this site")
+st.image("Image.jfif", caption="DCF Concept in this site")
